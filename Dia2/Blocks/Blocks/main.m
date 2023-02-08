8a2c1f8c6f8bfa5506226f905f13a27047e82738
Aldo Navarrete <aaldiitoo@gmail.com>
2023-02-08 13:20:28 -0600

    Categories and Blocks

--- Dia2/Blocks/Blocks/main.m
+++ Dia2/Blocks/Blocks/main.m
@@ -7,29 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
-
-
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Bienvenido a blocks!");
-
-        // Ejemplo 1
-        int anInteger = 42;
-        void (^testBlock)(void) = ^{
-            NSLog(@"Integer is: %i", anInteger);
-        };
-        anInteger = 84;
-        testBlock();
-
-        // Ejemplo 2
-        __block int aSecondInteger = 42;
-        void (^testBlock2)(void) = ^{
-           NSLog(@"Integer is: %i", aSecondInteger);
-           
-        };
-        aSecondInteger = 84;
-        testBlock2();
+        printf("Bienvenido a Blocks");
         
     }
     return 0;
--- Dia2/CategoriesAndBlocks/CategoriesAndBlocks/main.m
+++ Dia2/CategoriesAndBlocks/CategoriesAndBlocks/main.m
@@ -9,7 +9,6 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
     }
     return 0;
