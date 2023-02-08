e58ce27d04f9bd3eb99299d20cb8e715ae95d0ec
Aldo Navarrete <aaldiitoo@gmail.com>
2023-02-08 16:48:09 -0600

    Blocks

--- Dia2/Blocks/Blocks/main.m
+++ Dia2/Blocks/Blocks/main.m
@@ -7,9 +7,73 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Tester : NSObject
+
+@property (nonatomic, copy) void (^block)(id, NSUInteger, BOOL *);
+
+- (void) runTest;
+- (void) doSomethingWithBlock: (void (^)(id, NSUInteger, BOOL *)) block;
+- (void) funcionAsincrona;
+@end
+
+@implementation Tester
+
+- (void) runTest {
+    [self doSomethingWithBlock: ^(id obj, NSUInteger idx, BOOL *stop){
+        NSLog(@"%@", obj);
+        NSLog(@"Finalizado!");
+    }];
+}
+
+- (void) doSomethingWithBlock: (void (^)(id, NSUInteger, BOOL *)) block {
+    self.block = block;
+    [self performSelectorOnMainThread:@selector(funcionAsincrona) withObject:nil waitUntilDone:YES];
+}
+- (void) funcionAsincrona {
+    BOOL stop;
+    self.block(@"COD3", 0, &stop);
+}
+
+@end
+
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        printf("Bienvenido a Blocks");
+        
+        __block int unEntero = 10;
+        
+        // testBlock retorne nada y que reciba
+        void (^testBlock)(void) = ^{
+            NSLog(@"%d", unEntero);
+            unEntero++;
+            NSLog(@"%d", unEntero);
+            
+        };
+        
+        testBlock();
+        
+        // myBlock id, NSUInteger, BOOL *, void retorno.
+        void (^myBlock) (id, NSUInteger idx, BOOL *stop) = ^(id obj, NSUInteger idx, BOOL *stop) {
+            NSLog(@"Videojuego: %@", (NSString *) obj);
+            
+        };
+        BOOL stop;
+        myBlock(@"God of War", 0, &stop);
+        
+        // Pasarle un bloque a un metodo
+        NSArray *videojuegos = @[@"God of War", @"OverWatch", @"COD",@"Halo"];
+        [videojuegos enumerateObjectsUsingBlock:myBlock];
+        
+        // Inline form - forma linea
+        [videojuegos enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSLog(@"Videojuego 🎮: %@",(NSString *) obj);
+        }];
+        
+        // Probar bloques 'asíncrono'
+        Tester *tester = [Tester new]; // [[Tester alloc] init]
+        [tester runTest];
         
     }
     return 0;
