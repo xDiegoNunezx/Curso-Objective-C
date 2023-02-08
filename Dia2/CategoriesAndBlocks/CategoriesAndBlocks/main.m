b4b8cdbb4f5490554e29ad2e09bc2636f15d1d4e
Aldo Navarrete <aaldiitoo@gmail.com>
2023-02-08 08:46:08 -0600

    blocksAndCategories base

---
+++ Dia2/CategoriesAndBlocks/CategoriesAndBlocks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CategoriesAndBlocks
+//
+//  Created by Aldo Navarrete on 08/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
